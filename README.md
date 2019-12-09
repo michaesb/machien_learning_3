9d7ee0f4d9c85656546091d41dcf8fa733d0a24c
Michael BItney <michaesb@student.matnat.uio.no>
2019-12-09 15:03:11 +0100

    added more work on the decisiontree in order to determine a good ratio between fraud and non fraud

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # machine_learning_3
-Machine learning project in FYS-STK-4155/3155 looking into credit card fraud detection
+Machine learning project in FYS-STK4155/3155 looking into credit card fraud detection
+
+
+
+
+
+
+
+We have made a testfunction for the data processor, but since we used scikitlearns
+packages which has been heavily tested, we have not created test for these.
+In order to run the test execute the command below
+```
+pytest -v
+```
+
+
+
+
+### Authors
+
+* **Michael Bitney**
+* **Magnus Grøndalen**
--- src/neuralnetwork.py
+++ src/neuralnetwork.py
@@ -15,11 +15,11 @@ runs the classifier class with data from the data package
 """
 
 def neuralnet_clf_sklearn():
-    ratio_ = 0.25
+    ratio_ = 0.1
     X_train, X_test, y_train, y_test = retrieve_data(undersampling = True, ratio= ratio_)
 
 
-    learning_rate = np.linspace(0.01, 0.001, 11)
+    learning_rate = 10**(-np.linspace(0, 6, 70))
     n = len(learning_rate)
     timer = np.zeros(n)
     acc_score = np.zeros(n)
@@ -27,7 +27,7 @@ def neuralnet_clf_sklearn():
     prec_score = np.zeros(n)
     for i in range(len(learning_rate)):
         time1 = time.time()
-        print(int(100*i/len(learning_rate)), "%")
+        print(int(100*i/len(learning_rate)), "%", end = "\r")
         clf = sklearn.neural_network.MLPClassifier(
                                 hidden_layer_sizes = (10,10),
                                 learning_rate = "adaptive",
@@ -44,13 +44,16 @@ def neuralnet_clf_sklearn():
         time2 = time.time()
         timer[i] =time2 -time1
         # print("time = ",timer[i]," s")
-    plt.semilogx(learning_rate,acc_score, "*")
+
     plt.semilogx(learning_rate,acc_score)
+    plt.semilogx(learning_rate,prec_score)
+    plt.semilogx(learning_rate,rec_score)
+    plt.legend(['accuracy', "precision", "recall"])
     plt.xlabel(r"Learning rate $\eta$")
     plt.ylabel("Accuracy score")
     plt.title("Scikit-Learn NeuralNet score for different learning rates")
     plt.show()
-    print("ratio: ", ratio)
+    print("ratio: ", ratio_)
     print("accuracy_score",acc_score)
     print("precision_score",prec_score)
     print("rec_score",rec_score)
