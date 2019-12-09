737b2f312d17c2e348f01ff7813b0488146a566a
Magnus Grøndalen <magnus.bg@hotmail.com>
2019-12-09 17:09:25 +0100

    Finished grid search for random forest

--- src/randomforest.py
+++ src/randomforest.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score, recall_score, precision_score
+from sklearn.metrics import accuracy_score, recall_score, precision_score, make_scorer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from data_process import retrieve_data
@@ -13,24 +13,24 @@ clf = RandomForestClassifier(random_state=4)
 
 ## Grid search
 param_grid = {
-    'n_estimators' : [10, 100, 200],
-    'min_samples_split': [2, 3, 5, 10], 
-    'max_depth': [3, 5, 15, 25],
-    'max_features': [5, 10, 20, 30]
+    "n_estimators" : [10, 100, 200],
+    "min_samples_split": [2, 3, 5, 10], 
+    "max_depth": [3, 5, 15, 25],
+    "max_features": [5, 20, 30, "auto", "sqrt", "log2"]
 }
 
-from sklearn.metrics import make_scorer
 scorers = {
-    'precision_score': make_scorer(precision_score),
-    'recall_score': make_scorer(recall_score),
-    'accuracy_score': make_scorer(accuracy_score)
+    "precision_score": make_scorer(precision_score),
+    "recall_score": make_scorer(recall_score),
+    "accuracy_score": make_scorer(accuracy_score)
 }
-grid_search = GridSearchCV(clf, param_grid, scoring=scorers, refit='recall_score', return_train_score=True, n_jobs=-1)
+grid_search = GridSearchCV(clf, param_grid, cv=5, scoring=scorers, refit="recall_score", return_train_score=True, n_jobs=-1)
 grid_search.fit(X_train, y_train)
-prediction = grid_search.predict(X_test)
-print("Best params for Recall Score")
-print(grid_search.best_params_)
 
+prediction = grid_search.predict(X_test)
+print("Best params for Recall Score", grid_search.best_params_)
+
+# clf = RandomForestClassifier(random_state=4, **grid_search.best_params_)
 # clf.fit(X_train, y_train)
 # prediction = clf.predict(X_test)
 
