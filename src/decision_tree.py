3089b093136398547ffeac2223758524ac072d37
Magnus Grøndalen <magnus.bg@hotmail.com>
2019-12-18 02:00:36 +0100

    Deleted notebook

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -1,66 +1,69 @@
 #!/usr/bin/env python
-from sklearn.metrics import accuracy_score, recall_score, precision_score, make_scorer, confusion_matrix
+
+## Importing needed packages and methods
+from sklearn.metrics import accuracy_score, recall_score, precision_score, make_scorer
 from sklearn.model_selection import GridSearchCV
-from data_process import retrieve_data
-from plotting import plotting_ratio
 import matplotlib.pyplot as plt
-import scikitplot as skplt
-from scoring import scores
 from sklearn import tree
-import seaborn as sb
 import numpy as np
 import graphviz
-import time
 
-"""
-runs the classifier class with data from the data package
-"""
+## Importing own helper funcions
+from data_process import retrieve_data
+from scoring import scores
 
-def ratio_decisiontree():
-    # ratio_ = 0.25
+
+
+def decisiontree_undersamplingratio():
+    """
+    This funcions purpose is to test how the scores vary when using different
+    undersampling ratios and plots the results.
+    """
 
     n = 61
-    ratio_ = 10**(-np.linspace(0.5, 0.0, n))
-    # ratio_ = np.arange(1,101)/100
-    print("ratio: ", ratio_)
+
+    ratio_ = 10**(-np.linspace(6.0, 0.0, n))
     n = len(ratio_)
-    timer = np.zeros(n)
     acc_score = np.zeros(n)
     rec_score = np.zeros(n)
     prec_score = np.zeros(n)
     for i in range(n):
-        X_train, X_test, y_train, y_test = retrieve_data(undersampling = True, ratio = ratio_[i])
-        time1 = time.time()
+
         print(int(100*i/len(ratio_)), "%", end= "\r")
+        X_train, X_test, y_train, y_test = retrieve_data(undersampling = True, ratio = ratio_[i])
 
         clf = tree.DecisionTreeClassifier()
+
         clf = clf.fit(X_train, y_train.ravel())
         predict = clf.predict(X_test)
-        acc_score[i] = accuracy_score(y_test.ravel(),predict)
-        prec_score[i] = precision_score(y_test.ravel(),predict)
-        rec_score[i] = recall_score(y_test.ravel(),predict)
-        time2 = time.time()
-        timer[i] =time2 -time1
-        # print("time = ",timer[i]," s")
 
-        # import scikitplot as skplt
-        # skplt.metrics.plot_confusion_matrix(y_test, predict, normalize=True, hide_counts=False)
-        # plt.show()
+        acc_score[i] = accuracy_score(y_test.ravel(), predict)
+        prec_score[i] = precision_score(y_test.ravel(), predict)
+        rec_score[i] = recall_score(y_test.ravel(), predict)
 
-    plotting_ratio(ratio_, acc_score, prec_score, rec_score, "DecisionsTree")
-    print("accuracy_score",acc_score)
-    print("precision_score",prec_score)
-    print("rec_score",rec_score)
-
+    plt.semilogx(ratio_, acc_score)
+    plt.semilogx(ratio_, prec_score)
+    plt.semilogx(ratio_, rec_score)
+    plt.xlabel("Ratio", size=14)
+    plt.ylabel("Score", size=14)
+    plt.title("Scikit-Learn Decision Tree score for different ratios", size=16)
+    plt.legend(['Accuracy', "Precision", "Recall"], prop={'size': 12})
+    plt.savefig("plots/dectree_ratiotest.png")
+    plt.show()
 
 
-def grid_search_decisiontree():
+def decisiontree_gridsearch():
+    """
+    This function retrieves the dataset and uses grid search to find optimum parameters
+    to optimize the recall score of a Decision Tree classifier.
+    """
 
-    X_train, X_test, y_train, y_test = retrieve_data( undersampling=True, ratio=1.0, random_state=2 )
+    X_train, X_test, y_train, y_test = retrieve_data( undersampling=True, ratio=0.01, random_state=2 )
 
+    ## Our decision tree model
     clf = tree.DecisionTreeClassifier()
 
-    # Grid search
+    # Grid search parameter grid to search through.
     param_grid = {
         "criterion": ["gini","entropy"],
         "min_samples_split": [2, 3, 5, 8, 10], 
@@ -69,20 +72,14 @@ def grid_search_decisiontree():
         "min_samples_leaf": [1, 5, 10, 20, 50]
     }
 
-
-    # Best params for Recall Score {'criterion': 'gini', 'max_depth': 20, 'max_features': 30, 'min_samples_leaf': 1, 'min_samples_split': 2}
-    # Accuracy Test Score:   0.9108
-    # Precision Test Score:  0.8970. What percentage of the predicted frauds were frauds?
-    # Recall Test Score:     0.9250. What percentage of the actual frauds were predicted?
-    # Recall Train Score     1.0000
-    # Recall CV Train Score: 1.0000
-    # Recall CV Test Score:  0.9367
-
+    ## Different scorers for the grid search
     scorers = {
         "precision_score": make_scorer(precision_score),
         "recall_score": make_scorer(recall_score),
         "accuracy_score": make_scorer(accuracy_score)
     }
+
+    ## Creating the grid search object. Using refit="recall_score" to optimize using this score
     grid_search = GridSearchCV(clf, param_grid, cv=5, scoring=scorers, refit="recall_score", return_train_score=True, n_jobs=-1)
     
     grid_search.fit(X_train, y_train)
@@ -90,13 +87,19 @@ def grid_search_decisiontree():
 
     scores(prediction, y_test, X_train, y_train, grid_search)
 
+    ## Using the graphviz package to produce a PNG image to display the decision tree
     dot_data = tree.export_graphviz(grid_search.best_estimator_, out_file=None, filled=True, rounded=True,  special_characters=True)
     graph = graphviz.Source(dot_data)
     graph.format = "png"
     graph.render("tree")
 
+
     
-def decisiontree():
+def decisiontree_tuned():
+    """
+    This function reads the dataset and uses the decision tree ..
+    to test optimized parameters found in the function above.
+    """
 
     X_train, X_test, y_train, y_test = retrieve_data( undersampling=True, ratio=1, random_state=2 )
 
@@ -119,9 +122,11 @@ def decisiontree():
     graph.format = "png"
     graph.render("tree")
 
-# ratio_decisiontree()
 
-grid_search_decisiontree()
 
-# decisiontree()
+### Uncomment the function you'd like to run:
+
+# decisiontree_undersamplingratio()
+decisiontree_gridsearch()
+# decisiontree_tuned()
 
