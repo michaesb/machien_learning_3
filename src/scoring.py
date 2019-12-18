0f79f052c9f78a35dbfcbd14e593f39292088cab
Magnus Grøndalen <magnus.bg@hotmail.com>
2019-12-18 02:37:01 +0100

    Updated and commented helper function for scoring

--- src/scoring.py
+++ src/scoring.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+## Importing needed packages and methods
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 import matplotlib.pyplot as plt 
 import seaborn as sb
@@ -6,6 +9,13 @@ import numpy as np
 
 
 def scores(prediction, y_test, X_train, y_train, grid_search=None):
+    """
+    This is a helper function for handling all the scoring of the
+    different machine learning methods. The function prints accuracy score,
+    precision score and recall score, and also the CV scores when using 
+    grid search.
+    """
+
     if grid_search is not None:
         print("Best params for Recall Score", grid_search.best_params_)
 
@@ -28,9 +38,9 @@ def scores(prediction, y_test, X_train, y_train, grid_search=None):
     ax= plt.subplot()
     cm = confusion_matrix(y_test, prediction)
     sb.heatmap(cm, annot=True, ax = ax, fmt="g", cmap="Greens")
-    ax.set_xlabel("Predicted")
-    ax.set_ylabel("True")
-    ax.set_title("Confusion Matrix")
+    ax.set_xlabel("Predicted", size=14)
+    ax.set_ylabel("True", size=14)
+    ax.set_title("Confusion Matrix", size=16)
     ax.xaxis.set_ticklabels(["Non-fraud", "Fraud"])
     ax.yaxis.set_ticklabels(["Non-fraud", "Fraud"])
     plt.show()
